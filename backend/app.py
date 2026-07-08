30735676ca83e3c2b1d07f54732df876b11445c7
Anusha <anusha182006@gmail.com>
2026-07-09 00:54:01 +0530

    Update for Deployment

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from config import config
 
 from routes.upload import upload_bp
@@ -9,6 +10,8 @@ from routes.analyze import analyze_bp
 from routes.report import report_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
 app.register_blueprint(upload_bp)
@@ -17,11 +20,15 @@ app.register_blueprint(ioc_bp)
 app.register_blueprint(analyze_bp)
 app.register_blueprint(report_bp)
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return jsonify({
         "message": "SOC Copilot Running"
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(
+        host=config.HOST,
+        port=config.PORT,
+        debug=config.DEBUG
+    )
